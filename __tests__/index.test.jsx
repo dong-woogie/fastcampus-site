b4467a17e3301da869e5a67613553c49bf399d4f
FIFTEENH_woogi <fifteenh_woogi@FIFTEENH-woogiui-MacBookPro.local>
2022-10-17 00:01:02 +0900

    simple test code

--- __tests__/index.test.jsx
+++ __tests__/index.test.jsx
@@ -1,17 +1,35 @@
 // __tests__/index.test.jsx
 
-import { render, screen } from "@testing-library/react";
+import { render } from "@testing-library/react";
 import Home from "../pages/index";
 import "@testing-library/jest-dom";
 
 describe("Home", () => {
-  it("renders a heading", () => {
-    render(<Home />);
-
-    const heading = screen.getByRole("heading", {
-      name: /welcome to next\.js!/i,
+  it("카테고리 메뉴가 렌더링 되어야한다.", () => {
+    const { getByRole } = render(<Home />);
+    const navigation = getByRole("navigation", {
+      name: /fastcampus/g,
     });
 
-    expect(heading).toBeInTheDocument();
+    expect(navigation).toBeInTheDocument();
+  });
+
+  it("배너가 렌더링 되어야한다.", () => {
+    const { getByRole } = render(<Home />);
+    const banner = getByRole("banner", {
+      name: "",
+    });
+
+    expect(banner).toBeInTheDocument();
+  });
+
+  it("강의 목록이 렌더링 되어야한다.", () => {
+    const { getByTitle } = render(<Home />);
+
+    const lectureList = getByTitle("lectureList", {
+      name: "",
+    });
+
+    expect(lectureList).toBeInTheDocument();
   });
 });
